4962b7a301bf11c4125504c598f2ac44c4bb47a8
Kevin <k.lassauniere@live.fr>
2022-04-04 13:29:11 +0200

    Add score

--- main.rb
+++ main.rb
@@ -1,29 +1,30 @@
 # TODO 
-# Add score
 # Add choice for player name
 # Add randomize first player
 # Split main file into classes files
 
 class Player
-  attr_reader :name, :mark
+  attr_reader :name
+  attr_accessor :score
 
   @@first_player = true
   def initialize
-    @@first_player ? (@name = "player1"; @mark = "X") : (@name = "player2"; @mark = "O")
+    @@first_player ? (@name = "player1"; @score = 0) : (@name = "player2"; @score = 0)
     @@first_player = false
   end
 end
 
 class Game
   attr_reader :player1, :player2
-  
+
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @@current_player = player1
+    @current_player = player1
+    @round_nbr = 1
   end
 
-  def display
+  def display_board
     puts ''
     puts @board[0..2].join(' | ')
     puts '---------'
@@ -44,25 +45,25 @@ class Game
   end
 
   def play_turn
-    display
+    display_board
     position = get_input
     update_board(position)
     switch_player
   end
 
   def get_input
-    puts "Pick a position #{@@current_player.name}"
+    puts "Pick a position #{@current_player.name}"
     position = gets.chomp
     @board.include?(position) ? (return position) : (puts 'Wrong input'; get_input)
   end
 
   def update_board(position)
     pos_index = @board.index(position)
-    @@current_player == player1 ? (@board[pos_index] = "X") : (@board[pos_index] = "O")
+    @current_player == player1 ? (@board[pos_index] = "X") : (@board[pos_index] = "O")
   end 
 
   def switch_player
-    @@current_player == player1 ? (@@current_player = player2) : @@current_player = player1
+    @current_player == player1 ? (@current_player = player2) : @current_player = player1
   end
 
   def victory?
@@ -91,10 +92,12 @@ class Game
   end
 
   def finished_game
-    display
+    display_board
     if victory?
       switch_player # Need it to 'revert to previous' player
-      puts "#{@@current_player.name} wins !! Congratulations!"
+      puts "#{@current_player.name} wins !! Congratulations!"
+      update_score
+      display_score
     else
       puts "It's a draw !"
     end
@@ -102,14 +105,25 @@ class Game
     replay
   end
 
+  def update_score
+    @current_player.score += 1
+  end
+
+  def display_score
+    puts "#{player1.name}: #{player1.score} -- #{player2.name}: #{player2.score}"
+  end
+
   def replay
     puts "Play another round ? (Y/N)"
     answer = gets.chomp
     if answer == 'Y'
-      puts "Let's go !"
+      @round_nbr += 1
+      puts "Let's go for round #{@round_nbr}"
       play_round
     elsif answer == 'N'
       puts "Bye"
+      puts "Final score"
+      display_score
     else
       puts "Wrong input" 
       replay
